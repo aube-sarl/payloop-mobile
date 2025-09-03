79378739f73a43203063b726c63072c2c767f907
Tshobohwa <stshobohwa@gmail.com>
2025-09-03 22:22:25 +0300

    Create Send money form

---
+++ components/forms/send-money-form.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export default function SendMoneyForm() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: { backgroundColor: "white", width: "100%", height: 300 },
+});
---
+++ constants/screen-dimensions.ts
@@ -0,0 +1,9 @@
+const height = Dimensions.get("window").height;
+const width = Dimensions.get("window").width;
+
+const ScreenDimensions = {
+  height,
+  width,
+};
+
+export default ScreenDimensions;
