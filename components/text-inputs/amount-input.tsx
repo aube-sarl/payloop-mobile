0b3f221b4385c80a5aafcc62a49efb827b25258b
Tshobohwa <stshobohwa@gmail.com>
2025-09-05 14:49:45 +0300

    Add currency button to Amount input

--- components/text-inputs/amount-input.tsx
+++ components/text-inputs/amount-input.tsx
@@ -1,10 +1,22 @@
+import { congoFlag } from "@/assets/icons/_icons";
 import React from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-export default function AmountInput() {
+export default function AmountInput(props: { icon: any; currency: string }) {
   return (
     <View style={styles.container}>
       <TextInput style={styles.textInput} inputMode="decimal" />
+      <TouchableOpacity
+        style={{ backgroundColor: "white", borderLeftWidth: 1 }}
+      >
+        <Image source={congoFlag} style={{ height: 30, width: 30 }} />
+      </TouchableOpacity>
     </View>
   );
 }
